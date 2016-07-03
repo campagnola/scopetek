aca3b39d16a9fc8738de5beca06c95603bc80ec6
Luke Campagnola <luke.campagnola@gmail.com>
2016-07-02 23:36:06 -0700

    Got basic config / image acq working. Still some bug in video streaming.

--- test.py
+++ test.py
@@ -5,9 +5,12 @@ https://github.com/walac/pyusb/blob/master/docs/tutorial.rst
 
 
 """
-
 import sys
 import usb.core
+import pyqtgraph as pg
+import numpy as np
+
+
 dev = usb.core.find(idVendor=0x547)
 if dev is None:
     print "No cameras found!"
@@ -35,4 +38,59 @@ except usb.core.USBError as err:
         print permission_error_msg
         sys.exit(-1)
 
+
+
+# basic setup for 640x480
+setup = [
+    (0x40, 180, 0xc8 , 0   , 10,  0           ),   # set resolution
+    (0x40, 179, 0    , 0   , 10,  0           ),   # queue next frame?
+    (0x40, 183, 0x19 , 0x6 , 0 ,  0           ),
+    (0xc0, 182, 0    , 0x11, 10,  b'\x24\x03' ),
+    (0x40, 181, 0xa1 , 0   , 10,  0           ),   # set normal/fast frame speed mode
+    (0x40, 183, 0x28 , 0x35, 10,  0           ),   
+    (0x40, 183, 0x34 , 0x9 , 10,  0           ),
+    (0x40, 181, 0xa0 , 0   , 10,  0           ),
+    (0x40, 183, 0x10 , 0x35, 10,  0           ),   # set gain
+    (0x40, 183, 0xbf7, 0x9 , 10,  0           ),   # set exposure
+]
+for ct in setup:
+    if ct[0] & 0x80 > 0:
+        dev.ctrl_transfer(bmRequestType=ct[0], bRequest=ct[1], wValue=ct[2], wIndex=ct[3], data_or_wLength=ct[5])
+        print ct
+    else:
+        result = dev.ctrl_transfer(bmRequestType=ct[0], bRequest=ct[1], wValue=ct[2], wIndex=ct[3])
+        print ct, '==>', result
+
+
+imv = pg.image()
+
+while True:
+    # read one frame
+    rawdata = ep.read(640*480 + 512)
+    
+    
+    # Colors are in bayer filter pattern:
+    #   RGRGRG
+    #   GBGBGB
+    #   RGRGRG
+    #   GBGBGB
+    d1 = np.array(rawdata)
+    d2 = d1[512:].reshape(480, 640)
+    imv.setImage(d2.T)
+    # convert to RGB
+    #d3 = np.empty((480, 640, 3), dtype='ubyte')
+    #d3[::2, ::2, 0] = d2[::2, ::2]
+    #d3[::2, ::2, 1] = d2[1::2, ::2]
+    #d3[::2, ::2, 2] = d2[1::2, 1::2]
+    #d3[1::2, ::2] = d3[::2, ::2]
+    #d3[:, 1::2] = d3[:, ::2]
+    #imv.setImage(d3.transpose(1, 0, 2))
+
+    pg.QtGui.QApplication.processEvents()
+    
+    if not imv.isVisible():
+
+        # stop camera
+        dev.ctrl_transfer(bmRequestType=0x40, bRequest=187, wValue=0, wIndex=0)
+        break
 
