22b1b5faf4e74991d053f88194b4f05c8ccd8db0
Luke Campagnola <luke.campagnola@gmail.com>
2016-07-04 00:31:09 -0700

    better demosaicing faster frame streaming

--- scopetek.py
+++ scopetek.py
@@ -6,6 +6,9 @@ https://github.com/walac/pyusb/blob/master/docs/tutorial.rst
 
 """
 import sys
+import threading
+import time
+import Queue
 import usb.core
 import numpy as np
 
@@ -21,7 +24,11 @@ To fix this on Linux:
 """
 
 class Scopetek:
+    """Driver for scopetek / amscope / anchor microscope cameras. 
     
+    Tested on:
+        0547:c004  5MP-B CMOS Camera
+    """
     def __init__(self):
         dev = usb.core.find(idVendor=0x547)
         if dev is None:
@@ -41,6 +48,10 @@ class Scopetek:
         
         self.dev = dev
         self.ep = ep
+        self._last_frame = None
+        self._last_frame_time = None
+        self._fps = None
+        self.white_balance = [1, 1, 1]
 
     def setup(self, resolution, exposure, fast, gain):
         # translate to packet arguments
@@ -57,7 +68,15 @@ class Scopetek:
             exp = int(exposure * 11694)
             fst = 0xa0
         gn = gain + 0x0f
-
+        
+        self._setup(res, exp, fst, gn)
+        
+        self.resolution = resolution
+        self.fast = fast
+        self.exposure = exposure
+        self.gain = gain
+        
+    def _setup(self, res, exp, fst, gn):
         # basic control transfer sequence needed to configure and start the camera
         setup_ctrl = [
             (0x40, 180, res  , 0   , 10,  0           ),   # set resolution
@@ -78,37 +97,149 @@ class Scopetek:
                 self.dev.ctrl_transfer(bmRequestType=ct[0], bRequest=ct[1], wValue=ct[2], wIndex=ct[3], data_or_wLength=ct[5])
             else:
                 result = self.dev.ctrl_transfer(bmRequestType=ct[0], bRequest=ct[1], wValue=ct[2], wIndex=ct[3])
-                
-        self.resolution = resolution
-        self.fast = fast
-        self.exposure = exposure
-        self.gain = gain
 
     def stop(self):
         self.dev.ctrl_transfer(bmRequestType=0x40, bRequest=187, wValue=0, wIndex=0)
 
-    def readframe(self):
+    def read_frame(self):
         """Read one frame from the video stream.
         
-        # Colors are in bayer filter pattern:
-        #   RGRGRG
-        #   GBGBGB
-        #   RGRGRG
-        #   GBGBGB
+        Colors are in bayer filter pattern:
+            RGRGRG
+            GBGBGB
+            RGRGRG
+            GBGBGB
         """
         # read one frame
         extra = 256 if self.resolution[0] == 2592 else 512
         data = self.ep.read(self.resolution[0] * self.resolution[1] + extra)
-        
-        # queue the next frame
+
+        # queue the next frame immediately
         self.dev.ctrl_transfer(bmRequestType=0x40, bRequest=179, wValue=0, wIndex=0)
         
+        # make a Frame instance
         d1 = np.array(data)
         d2 = d1[extra:].reshape(self.resolution[1], self.resolution[0])
-        return d2
+        frame = Frame(d2, wb=self.white_balance)
+
+        # measure FPS
+        now = frame.time
+        if self._last_frame_time is not None:
+            self._fps = 1.0 / (now - self._last_frame_time)
+        self._last_frame_time = now
+        
+        self._last_frame = frame
+        return frame
+        
+    def auto_white_balance(self):
+        if self._last_frame is None:
+            self.read_frame()
+        lf = self._last_frame.image()
+        r, g, b = np.median(lf[...,0]), np.median(lf[...,1]), np.median(lf[...,2])
+        mx = max([r, g, b])
+        self.white_balance = [mx/r, mx/g, mx/b]
+
+
+class StreamThread(threading.Thread):
+    def __init__(self, cam):
+        self.cam = cam
+        self._stop = False
+        self.stop_lock = threading.Lock()
+        self.frames = Queue.Queue()
+        threading.Thread.__init__(self)
+        self.daemon = True
+        self._last_frame_time = None
+
+    def start(self):
+        with self.stop_lock:
+            self._stop = False
+        threading.Thread.start(self)
+
+    def run(self):
+        while True:
+            with self.stop_lock:
+                if self._stop is True:
+                    break
+            frame = self.cam.read_frame()
+            self.frames.put(frame)
     
+    def stop(self):
+        with self.stop_lock:
+            self._stop = True
+
+    def get_first(self):
+        """Return the oldest frame in the queue, or None if the queue is empty.
+        """
+        try:
+            return self.frames.get(block=False)
+        except Queue.Empty:
+            return None
+        
+    def get_all(self):
+        """Remove all frames from the queue ant return them in a list.
+        """
+        frames = []
+        while True:
+            try:
+                frames.append(self.frames.get(block=False))
+            except Queue.Empty:
+                break
+        return frames
+
+
+class Frame(object):
+    def __init__(self, data, wb=None):
+        self.time = time.time()
+        self.data = data
+        self.rgb = None
+        if wb is None:
+            self.white_balance = [1, 1, 1]
+        else:
+            self.white_balance = wb
+        
+    def image(self):
+        if self.rgb is None:
+            self.rgb = self.apply_wb(self.bayer_to_rgb(self.data))
+        return self.rgb    
+        
+    def apply_wb(self, img):
+        wb = img * np.array(self.white_balance).reshape(1, 1, 3)
+        return wb
+
     @staticmethod
     def bayer_to_rgb(d2):
+        # R G R G R G
+        # g B g B g B
+        # R G R G R G
+        # g B g B g B
+        # R G R G R G
+        # g B g B g B
+        d2 = d2.astype('uint16')
+        
+        r = d2[::2, ::2]
+        g1 = d2[1::2, ::2]
+        g2 = d2[::2, 1::2]
+        b = d2[1::2, 1::2]
+        
+        d3 = np.zeros(d2.shape + (3,), dtype='ubyte')
+        d3[::2, ::2, 0] = r
+        d3[1:-1:2, ::2, 0] = (r[:-1, :] + r[1:, :]) // 2
+        d3[::2, 1:-1:2, 0] = (r[:, :-1] + r[:, 1:]) // 2
+        d3[1:-1:2, 1:-1:2, 0] = (r[:-1, :-1] + r[1:, :-1] + r[:-1, 1:] + r[1:, 1:]) // 4
+        
+        d3[1::2, ::2, 1] = g1
+        d3[::2, 1::2, 1] = g2
+        d3[1:-1:2, 1:-1:2, 1] = (g1[:-1, :-1] + g1[:-1, 1:] + g2[:-1, :-1] + g2[1:, :-1]) // 4
+        d3[2::2, 2::2, 1] = (g1[:-1, 1:] + g1[1:, 1:] + g2[1:, :-1] + g2[1:, 1:]) // 4
+        
+        d3[1::2, 1::2, 2] = b
+        d3[1::2, 2::2, 2] = (b[:, :-1] + b[:, 1:]) // 2
+        d3[2::2, 1::2, 2] = (b[:-1, :] + b[1:, :]) // 2
+        d3[2::2, 2::2, 2] = (b[:-1, :-1] + b[1:, :-1] + b[:-1, 1:] + b[1:, 1:]) // 4 
+        return d3
+
+    @staticmethod
+    def fast_bayer_to_rgb(d2):
         d3 = np.empty(d2.shape + (3,), dtype='ubyte')
         d3[::2, ::2, 0] = d2[::2, ::2]
         d3[::2, ::2, 1] = d2[1::2, ::2]
@@ -117,3 +248,4 @@ class Scopetek:
         d3[:, 1::2] = d3[:, ::2]
         return d3
 
+    
--- video_test.py
+++ video_test.py
@@ -1,20 +1,33 @@
 import pyqtgraph as pg
-from scopetek import Scopetek
+from scopetek import Scopetek, StreamThread
 
 # Initialize camera
 cam = Scopetek()
 # Set camera parameters
-cam.setup(resolution=(640,480), fast=True, exposure=131e-3, gain=1)
+cam.setup(resolution=(640,480), fast=True, exposure=90e-3, gain=1)
+cam.auto_white_balance()
 
 imv = pg.image()
 
-    
-while True:
-    frame = cam.readframe()
-    rgb = cam.bayer_to_rgb(frame)
-    imv.setImage(rgb.transpose(1, 0, 2))
-    
-    pg.QtGui.QApplication.processEvents()
+stream = StreamThread(cam)
+stream.start()
+
+timer = pg.QtCore.QTimer()
+def update():
     if not imv.isVisible():
+        stream.stop()
+        timer.stop()
         cam.stop()
-        break
+        return
+    
+    # Empty out the entire queue; drop frames as needed.
+    frames = stream.get_all()
+    if len(frames) == 0:
+        return
+    
+    imv.window().setWindowTitle('%0.1f FPS' % cam._fps)
+    imv.setImage(frames[-1].image()[1:-1, 1:-2].transpose(1, 0, 2), autoRange=False)
+
+        
+timer.timeout.connect(update)
+timer.start(30)
